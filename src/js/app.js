18c42a067c59666cbf283b6cb059864fdf6f60d3
Dima Shirkov <dimashirkov@list.ru>
2023-04-12 17:47:39 +0300

    first commit

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,4 @@
-// TODO: write your code here
-import sum from './basic';
-
 document.addEventListener('DOMContentLoaded', () => {
-/*
-весь код приложения внутри обработчика
-данного события начнет работать после загрузки DOM
-*/
 
-  console.log('worked');
-  console.log(sum([1, 2]));
+
 });
