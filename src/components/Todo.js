59c2ee2ef02ce58628cf4be4e9038bd10c46b528
Dima Shirkov <dimashirkov@list.ru>
2023-04-14 19:03:55 +0300

    add card input field display

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,9 +1,38 @@
 export default class Todo{
     constructor(){
         this.boardTask = [];
+        this.fieldForAddCard;
+        this.textInputField;
+
     }
 
-    addTask(){
-        console.log('hi');
+    showInputField(element){
+        this.fieldForAddCard = element;
+        this.textInputField = element.parentElement.parentElement.firstElementChild;
+
+        this.fieldForAddCard.classList.add('hide_element');
+        this.textInputField.classList.add('show_element');
+
+        this.cardFormation();
+    }
+
+    cardFormation(){
+        const textArea = this.textInputField.querySelector('.task-input-field');
+
+        textArea.addEventListener('keyup', () => {
+            textArea.style.height = '0px';
+            textArea.style.height = textArea.scrollHeight + 'px';
+        })
+
+        const buttonAddCard = this.textInputField.querySelector('.footer-button-add-card');
+        
+        this.textInputField.addEventListener('click', (event) => {
+            let buttonClose = event.target;
+            if(buttonClose.classList.contains('footer-button-add-card') 
+            || buttonClose.classList.contains('click-add-card-close')){
+                this.textInputField.classList.remove('show_element');
+                this.fieldForAddCard.classList.remove('hide_element')
+            }
+        })
     }
 }
--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,22 @@
 import Todo from "../components/Todo";
 
 document.addEventListener('DOMContentLoaded', () => {
-    const buttonAddCards = document.querySelectorAll('.button-icon');
     const todoList = new Todo();
+    const buttonAddCards = document.querySelectorAll('.button-icon');
 
-    buttonAddCards.forEach(item => item.addEventListener('click', todoList.addTask));
+    buttonAddCards.forEach(button => {
+        button.addEventListener('click', () => todoList.showInputField(button))
+    });
+
+    // Стоит ли использовать всплытие??
+    // const mainContainer = document.querySelector('.wrapper-columns-boards');
+
+    // mainContainer.addEventListener('click', (event) => {
+    //     debugger
+    //     let buttonAddCards = event.target.closest('.button-icon');
+
+    //     if(!buttonAddCards) return;
+
+    //     todoList.showInputField(buttonAddCards);
+    // })
 });
