d87fa747dba9b05f6f5e6e2a8ed18de9128ebbd0
Dima Shirkov <dimashirkov@list.ru>
2023-04-14 22:04:56 +0300

    add sending a card to the board

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,38 +1,63 @@
 export default class Todo{
     constructor(){
         this.boardTask = [];
-        this.fieldForAddCard;
-        this.textInputField;
+        this.showTextarea;
+        this.textarea;
 
     }
 
     showInputField(element){
-        this.fieldForAddCard = element;
-        this.textInputField = element.parentElement.parentElement.firstElementChild;
+        this.showTextarea = element.parentElement; // кнопка открытия поля
+        this.textarea = element.parentElement.parentElement.firstElementChild;
+        // само поле ввода
 
-        this.fieldForAddCard.classList.add('hide_element');
-        this.textInputField.classList.add('show_element');
+        this.showTextarea.classList.add('hide_element');
+        this.showTextarea.classList.remove('footer-block-card');
+
+        this.textarea.classList.add('show_element');
+        this.textarea.classList.remove('hide_element');
 
         this.cardFormation();
     }
 
     cardFormation(){
-        const textArea = this.textInputField.querySelector('.task-input-field');
+        const textArea = this.textarea.querySelector('.task-input-field');
 
         textArea.addEventListener('keyup', () => {
             textArea.style.height = '0px';
             textArea.style.height = textArea.scrollHeight + 'px';
         })
-
-        const buttonAddCard = this.textInputField.querySelector('.footer-button-add-card');
         
-        this.textInputField.addEventListener('click', (event) => {
+        this.textarea.addEventListener('click', (event) => {
             let buttonClose = event.target;
+
             if(buttonClose.classList.contains('footer-button-add-card') 
             || buttonClose.classList.contains('click-add-card-close')){
-                this.textInputField.classList.remove('show_element');
-                this.fieldForAddCard.classList.remove('hide_element')
+
+                debugger
+                if(buttonClose.classList.contains('footer-button-add-card')) this.cardDisplay(textArea);
+
+                this.textarea = buttonClose.closest('.show_element');
+                this.textarea.classList.remove('show_element');
+                this.textarea.classList.add('hide_element');
+
+                this.showTextarea = buttonClose.closest('footer').lastElementChild;
+                this.showTextarea.classList.remove('hide_element');
+                this.showTextarea.classList.add('footer-block-card');
             }
+
         })
+    }
+
+    cardDisplay(contentArea) {
+        console.log(contentArea)
+        // получить значение texarea
+        const card = document.querySelector('.card');
+
+        const newCard = card.cloneNode(true);
+        newCard.querySelector('.card-message').textContent = contentArea.textContent;
+
+        console.log(this.textarea)
+        console.log(newCard);
     }
 }
